69c2040f358ce22421ed0539023af60e51a265d2
Khoa Tran <ovn6002@gmail.com>
2025-04-20 18:38:19 +0700

    fix: process image in memory, no disk write

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
 from rembg import remove
+from io import BytesIO
 from PIL import Image
-import io
 import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
-OUTPUT_FOLDER = 'outputs'
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1014 * 1024 # 10MB Limit
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+# UPLOAD_FOLDER = 'uploads'
+# OUTPUT_FOLDER = 'outputs'
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-if not os.path.exists(OUTPUT_FOLDER):
-    os.makedirs(OUTPUT_FOLDER)
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+
+# if not os.path.exists(UPLOAD_FOLDER):
+#     os.makedirs(UPLOAD_FOLDER)
+# if not os.path.exists(OUTPUT_FOLDER):
+#     os.makedirs(OUTPUT_FOLDER)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -27,30 +30,32 @@ def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             return render_template('index.html', error='No file part')
+        
         file = request.files['file']
         if file.filename == '':
             return render_template('index.html', error='No selected file')
-        if file and allowed_file(file.filename):
-            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filename)
-            try:
-                output_buffer = io.BytesIO()
-                input_image = Image.open(filename)
-                output_image = remove(input_image)
-                output_image.save(output_buffer, format="PNG")
-                output_buffer.seek(0)
-                os.remove(filename)
-                return send_file(
-                    output_buffer,
-                    mimetype='image/png',
-                    as_attachment=True,
-                    download_name='removed_background.png'
-                )
-            except Exception as e:
-                os.remove(filename)
-                return render_template('index.html', error=f'Error processing image: {e}')
-        else:
-            return render_template('index.html', error='Allowed file types are png, jpg, jpeg')
+
+        if not allowed_file(file.filename):
+            return render_template('index.html', error='Invalid file extension')
+
+        try:
+            input_image = Image.open(file.stream)
+            output_buffer = BytesIO()
+            output_image = remove(input_image)
+            output_image.save(output_buffer, format='PNG')
+            output_buffer.seek(0)
+
+            return send_file(
+                output_buffer,
+                mimetype = 'image/png',
+                as_attachment = True,
+                download_name = f"{secure_filename(file.filename)}_bg_removed.png"
+            )
+
+        except Exception as e:
+            app.logger.error(f"Error processing image: {e}")
+            return render_template('index.html', error='Failed to process image')
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
